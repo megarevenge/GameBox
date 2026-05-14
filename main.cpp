80e460b3a8426eb3ff9c1e4f015b7d3648ae7000
Egamov Muhammad <egamovmuhammad.sr@gmail.com>
2026-05-14 22:03:13 +0500

    The End?🌀

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     SeaShipSelectingWindow p1;
     p1.fileName = "p1.dat";
-    p1.resize(800, 600);
     p1.setWindowTitle("Sea Battle");
     p1.show();
     return QCoreApplication::exec();
